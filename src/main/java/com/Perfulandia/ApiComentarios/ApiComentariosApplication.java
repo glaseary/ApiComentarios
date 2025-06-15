7ba8efbaa25bc71e17287ab38c6e4f5e3d3899ee
glaseary <sofia.troncoso.m@gmail.com>
2025-06-14 23:40:11 -0400

    primer commit

---
+++ src/main/java/com/Perfulandia/ApiComentarios/ApiComentariosApplication.java
@@ -0,0 +1,13 @@
+package com.Perfulandia.ApiComentarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComentariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComentariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Perfulandia/ApiComentarios/ApiComentariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Perfulandia.ApiComentarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComentariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
